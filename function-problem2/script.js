23003efa994909de8662c5fdef53842adf41c606
Darren Kim <dkim555@yahoo.com>
2023-01-02 19:57:45 -0800

    add if statement

--- function-problem2/script.js
+++ function-problem2/script.js
@@ -1,13 +1,20 @@
 // Write a JavaScript function that accepts a string as a parameter 
 // and counts the number of vowels within the string
 
-function findVowels(sentence) {
+function findVowels( sentence) {
 //define what a vowel is
     const vowels = ['a', 'e', 'i', 'o', 'u'];
 // turn the string into all lowercases and split them into individual letters
     const letters = sentence.toLowerCase().split('');
-// make another varible to have a starting point to count
+// make another varible to have a starting point to count 
     let numOfVowels = 0;
+//make a parameter called letter to go through each item in letters
+    letters.forEach(function(letter){
+        if (vowels.includes(letter)){
+            numOfVowels += 1;
+        }
+    })
+    return (numOfVowels)
 }
 
 findVowels ('HUMPTY DUMPTY SAT ON A WALL')

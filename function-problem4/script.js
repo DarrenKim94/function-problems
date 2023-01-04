62eb944d97f865dfe9fe7cf2f22fa9d1fcaf6edb
Darren Kim <dkim555@yahoo.com>
2023-01-03 20:42:49 -0800

    write comments

--- function-problem4/script.js
+++ function-problem4/script.js
@@ -2,17 +2,15 @@
 // three print "Fizz" instead of the number and for the multiples of five print "Buzz". For 
 // numbers that are multiples of both three and five print "FizzBuzz".
 
-// dont need an argument because we just need to call ghd function
-// Write a JavaScript function that iterates the integers from 1 to 100. But for multiples of 
-// three print "Fizz" instead of the number and for the multiples of five print "Buzz". For 
-// numbers that are multiples of both three and five print "FizzBuzz".
-
-// dont need an argument because we just need to call ghd function
+// dont need an argument because we just need to call the function
 function fizzBuzz () {
+// loop from 1 to 100 by 1
     for (let i = 1; i < 100; i++) {
+// write an if statement that makes condition true only if both modulos are true
         if (i % 3 === 0 && i % 5 === 0) {
             console.log(i, 'FizzBuzz');
         }
+// write else if statements seperatley to give them their own console.logs
         else if (i % 3 === 0) {
             console.log(i, 'fizz');
         }

9b4180e7796b89d984581047b9be54dd8a52b060
Darren Kim <dkim555@yahoo.com>
2023-01-03 19:58:44 -0800

    add for loop

---
+++ function-problem4/script.js
@@ -0,0 +1,9 @@
+// Write a JavaScript function that iterates the integers from 1 to 100. But for multiples of 
+// three print "Fizz" instead of the number and for the multiples of five print "Buzz". For 
+// numbers that are multiples of both three and five print "FizzBuzz".
+
+// dont need an argument because we just need to call ghd function
+function fizzBuzz () {
+    // loop to 100
+    for (let i = 0; i < 100; i++)
+}
